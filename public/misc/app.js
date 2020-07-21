cdb91c75bfcb1eec3e3688b0b61a3b6898556211
chengkangzai <pycck@hotmail.com>
2020-07-21 18:19:23 +0800

    add new feature

--- public/misc/app.js
+++ public/misc/app.js
@@ -10,15 +10,24 @@ $("#bladeInput").on('keyup', function() {
 $("#wordInput").on('keyup', function() {
     renderExcel();
 });
+$("#submitExcelInput").on('click', function() {
+    submitToFB();
+});
+
+$("#selectButton").on('click', function() {
+    renderSelection();
+});
 
 function resetAllBox() {
     $("#generateKey").hide();
     $("#generateExcel").hide();
+    $("#generateExcelToKey").hide();
 }
 
 function resetAllButton() {
     $("#btn-openGenerateKey").removeClass("btn-success").addClass("btn-outline-success")
     $("#btn-openGenerateExcel").removeClass("btn-warning").addClass("btn-outline-warning")
+    $("#btn-openGenerateExcelToKey").removeClass("btn-info").addClass("btn-outline-info")
 }
 
 $("#btn-openGenerateKey").on('click', function() {
@@ -43,6 +52,33 @@ $("#btn-openGenerateExcel").on('click', function() {
         div.css('display', 'block')
     } else {
         $("#btn-openGenerateExcel").addClass("btn-outline-warning").removeClass("btn-warning")
+        div.css('display', 'none')
+    }
+});
+var file = [];
+var dataFromDB = [];
+$("#btn-openGenerateExcelToKey").on('click', function() {
+    var div = $("#generateExcelToKey");
+    if (div.css('display') == "none") {
+        resetAllButton();
+        $("#btn-openGenerateExcelToKey").addClass("btn-info").removeClass("btn-outline-info")
+        resetAllBox();
+        div.css('display', 'block')
+            // initialize file and object
+        db
+            .collection("misc/phpKey/laravel")
+            .get()
+            .then((snap) => {
+                snap.forEach(function(doc) {
+                    file.push(doc.data().filename)
+                    dataFromDB.push(doc.data());
+                });
+            })
+            .catch((error) => {
+
+            })
+    } else {
+        $("#btn-openGenerateExcelToKey").addClass("btn-outline-info").removeClass("btn-info")
         div.css('display', 'none')
     }
 })
@@ -83,5 +119,93 @@ function renderExcel() {
     } else {
         $("#outputExcel").addClass("is-invalid").removeClass("is-valid")
         console.log("Diff key ");
+    }
+}
+
+function submitToFB() {
+    var input = $("#inputExcel").val();
+    var input2 = [];
+    input2 = input.split("\n");
+    console.log(input2.length);
+    var object = [];
+    for (let i = 0; i < input2.length; i++) {
+        var split = input2[i].split("\t")
+        if (split.length = 6) {
+            var object = {
+                filename: split[0],
+                key: split[1],
+                en: split[2],
+                zh: split[3],
+                th: split[4],
+                my: split[5]
+            }
+            console.log(object);
+            db
+                .collection("misc/phpKey/laravel")
+                .add(Object.assign({}, object))
+                .then((response) => {
+                    $("#inputExcel").addClass("is-valid").removeClass("is-invalid")
+                    $("#inputExcel").val("");
+                })
+                .catch((error) => {
+                    console.log(error)
+                });
+        } else {
+            $("#inputExcel").addClass("is-invalid").removeClass("is-valid")
+        }
+    }
+}
+
+function renderSelection() {
+    var uniqueItems = Array.from(new Set(file));
+    console.log(uniqueItems);
+    $("#fileSelection").replaceWith(`<select class="form-control mb-1" id="fileSelection" onchange='exportKey()'></select>`);
+    uniqueItems.forEach((value, index) => {
+        $("#fileSelection").append(`<option>${uniqueItems[index]}</option>`);
+    })
+}
+
+function exportKey() {
+
+    $("#excelZH").val("");
+    $("#excelTH").val("");
+    $("#excelMY").val("");
+    var keys = Array.from(new Set(dataFromDB));
+    var uniqueKey = [...new Set(dataFromDB.map(item => item.key))]; // [ 'A', 'B']
+
+    var zhDom = [],
+        thDom = [],
+        myDom = [];
+    for (const key in keys) {
+        if (keys.hasOwnProperty(key)) {
+            const element = keys[key];
+            if (element.filename == $("#fileSelection :selected").text()) {
+                const index = uniqueKey.indexOf(element.key);
+                if (index > -1) {
+                    zhDom.push(`\t'${element.key}' => '${element.zh}'`)
+                    thDom.push(`\t'${element.key}' => '${element.th}'`)
+                    myDom.push(`\t'${element.key}' => '${element.my}'`)
+                }
+                // Delete 
+                const index1 = uniqueKey.indexOf(element.key);
+                if (index1 > -1) {
+                    uniqueKey.splice(index1, 1);
+                }
+
+            }
+        }
+    }
+
+    $("#excelZH").val(zhDom.sort().join(", \n"));
+    $("#excelTH").val(thDom.sort().join(", \n"));
+    $("#excelMY").val(myDom.sort().join(", \n"));
+    if (zhDom.length !== 0 || thDom.length !== 0 || myDom.length !== 0) {
+        $("#excelZH").addClass("is-valid").removeClass("is-invalid");
+        $("#excelTH").addClass("is-valid").removeClass("is-invalid");
+        $("#excelMY").addClass("is-valid").removeClass("is-invalid");
+    } else {
+        $("#excelZH").removeClass("is-valid").addClass("is-invalid");
+        $("#excelTH").removeClass("is-valid").addClass("is-invalid");
+        $("#excelMY").removeClass("is-valid").addClass("is-invalid");
     }
 }
