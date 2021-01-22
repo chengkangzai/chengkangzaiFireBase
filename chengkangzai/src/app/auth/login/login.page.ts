e9ff0d5013bfbd5717e98afec33cd10549ae74ab
Ching Cheng Kang <pycck@hotmail.com>
2021-01-22 21:09:24 +0800

    split the auth to 2 part and identify phone platform (#9)

--- chengkangzai/src/app/auth/login/login.page.ts
+++ chengkangzai/src/app/auth/login/login.page.ts
@@ -3,7 +3,7 @@ import {AuthProvider} from "ngx-auth-firebaseui";
 import {Router} from "@angular/router";
 import {AngularFireAuth} from "@angular/fire/auth";
 import {Subscription} from "rxjs";
-
+import {Platform} from "@ionic/angular";
 
 @Component({
     selector: 'app-login',
@@ -14,12 +14,24 @@ export class LoginPage {
 
     providers = AuthProvider;
     authSub: Subscription;
+    isPhone = false;
 
-    constructor(private router: Router, private firebase: AngularFireAuth) {
+    constructor(
+        private router: Router,
+        private afAuth: AngularFireAuth,
+        private platform: Platform,
+    ) {
     }
 
     ionViewWillEnter() {
-        this.authSub = this.firebase.authState.subscribe(async user => {
+        if (
+            this.platform.is('hybrid') &&
+            (this.platform.is('android') || this.platform.is('ios'))
+        ) {
+            this.isPhone = true;
+        }
+
+        this.authSub = this.afAuth.authState.subscribe(async user => {
             if (user) {
                 await this.router.navigateByUrl('tabs/ngrok');
             }
@@ -32,7 +44,4 @@ export class LoginPage {
         }
     }
 
-    async onSuccess(event: Event) {
-        await this.router.navigateByUrl('tabs/ngrok')
-    }
 }
