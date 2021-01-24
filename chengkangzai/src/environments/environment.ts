a286a1873911cab6c11a30631f03c2610abc199f
chengkangzai <pycck@hotmail.com>
2021-01-25 00:09:10 +0800

    fix single quotation problem
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- chengkangzai/src/environments/environment.ts
+++ chengkangzai/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyBjwoB-wYslKQO3GXPmPC5O8uLXFZsXNIQ",
-        authDomain: "chengkangzai.firebaseapp.com",
-        databaseURL: "https://chengkangzai.firebaseio.com",
-        projectId: "chengkangzai",
-        storageBucket: "chengkangzai.appspot.com",
-        messagingSenderId: "769517222834",
-        appId: "1:769517222834:web:ca371a76889970cbeb02ab",
-        measurementId: "G-1CLH75RHL7"
+        apiKey: 'AIzaSyBjwoB-wYslKQO3GXPmPC5O8uLXFZsXNIQ',
+        authDomain: 'chengkangzai.firebaseapp.com',
+        databaseURL: 'https://chengkangzai.firebaseio.com',
+        projectId: 'chengkangzai',
+        storageBucket: 'chengkangzai.appspot.com',
+        messagingSenderId: '769517222834',
+        appId: '1:769517222834:web:ca371a76889970cbeb02ab',
+        measurementId: 'G-1CLH75RHL7'
     }
 };
 
