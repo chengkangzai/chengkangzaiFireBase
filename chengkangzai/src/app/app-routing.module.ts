2472a7cccac7ae4563a7b1018e516a846b3946cd
chengkangzai <pycck@hotmail.com>
2021-01-25 16:26:16 +0800

    change guard to firebase auth

--- chengkangzai/src/app/app-routing.module.ts
+++ chengkangzai/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
-import {LoggedInGuard} from 'ngx-auth-firebaseui';
+import {canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
+
+const redirectLoggedInToItems = () => redirectLoggedInTo(['tabs/ngrok']);
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
     {
@@ -10,11 +13,12 @@ const routes: Routes = [
     },
     {
         path: 'tabs',
-        canActivate: [LoggedInGuard],
+        ...canActivate(redirectUnauthorizedToLogin),
         loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
     },
     {
         path: 'login',
+        ...canActivate(redirectLoggedInToItems),
         loadChildren: () => import('./pages/auth/login/login.module').then(m => m.LoginPageModule)
     },
 
