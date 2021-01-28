15a8b797a1773d2a4c53c8623f73d73f36caaf77
chengkangzai <pycck@hotmail.com>
2021-01-28 15:15:45 +0800

    change icon for web and mobile
    Signed-off-by: chengkangzai <pycck@hotmail.com>

--- chengkangzai/src/app/model/feedback.spec.ts
+++ chengkangzai/src/app/model/feedback.spec.ts
@@ -3,7 +3,7 @@ import firebase from 'firebase/app';
 
 describe('Feedback', () => {
     it('should create an instance', () => {
-        expect(new Feedback('', new
+        expect(new Feedback('', '', new
             firebase.firestore.Timestamp(20000, 2000000), ''
         )).toBeTruthy();
     });
